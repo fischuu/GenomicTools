c25158bbb7d0825e19eb433b7a8ebf418571314e
fischuu <github@danielfischer.name>
2016-08-11 14:48:12 +0300

    Functions moved from hoardeR

--- R/importGTF.R
+++ R/importGTF.R
@@ -1,27 +1,37 @@
-importGTF <- function(gtf, skip=0, nrow=-1, use.data.table=FALSE){
+importGTF <- function(file, skip=0, nrow=-1, use.data.table=FALSE, level="gene"){
+  gtf <- file
   if(use.data.table){
-    out <- fread(input=gtf, sep="\t", header=FALSE, skip=skip, nrows=nrow)
+    cuffLoaded <- fread(input = paste('zcat',gtf), skip=5, colClasses = c("character",
+                                                                          "character",
+                                                                          "character",
+                                                                          "integer",
+                                                                          "integer",
+                                                                          "character",
+                                                                          "character",
+                                                                          "character",
+                                                                          "character"))
+    if(!is.null(level)) cuffLoaded <- cuffLoaded[cuffLoaded$V3==level,]
+    if(nrow>0) cuffLoaded <- cuffLoaded[1:nrow,]
   } else {
-    
-    # Load the file and split the Variable V9
-      cuffLoaded <- read.csv(file=gtf, sep="\t", header=FALSE, stringsAsFactors=FALSE, skip=skip, nrow=nrow)
-      V9 <- cuffLoaded$V9
-      V9 <- strsplit(V9,"; ")
-      
-    # Now get the part before the space (=first) and after (=second)
-      first <- sapply(V9, function(x)unlist(regmatches( x , gregexpr( "[_a-zA-Z]+ " , x ) )))
-      second <- sapply(V9, function(x)unlist(regmatches( x , gregexpr( "[_a-zA-Z0-9=.]+" , x ) )))
-        
-    # Column names
-      tags <- unique( unlist(first) )
-      
-    # Intermediate matrices
-      temp <- mapply( cbind , second , first )
-      
-    # Match to appropriate columns and coerce to data.frame
-      out <- data.frame( do.call( rbind , lapply( temp , function(x) x[ match( tags , x[,2] ) ]  ) ) , stringsAsFactors=FALSE)
-      names(out) <- tags
-      out <- cbind(cuffLoaded[,-9],out)
+    cuffLoaded <- read.csv(file=gtf, sep="\t", header=FALSE, stringsAsFactors=FALSE, skip=skip, nrow=nrow)
   }
-  out
+  # Split the variable V9
+    V9 <- cuffLoaded$V9
+    V9 <- strsplit(V9,"; ")
+      
+  # Now split the data
+    V9Splitted <- sapply(V9, function(x)unlist(x))
+        
+  # Column names
+    tags <- unique( unlist(first) )
+      
+  # Intermediate matrices
+    temp <- mapply( cbind , second , first )
+      
+  # Match to appropriate columns and coerce to data.frame
+    out <- data.frame( do.call( rbind , lapply( temp , function(x) x[ match( tags , x[,2] ) ]  ) ) , stringsAsFactors=FALSE)
+    names(out) <- tags
+    out <- cbind(cuffLoaded[,-9],out)
+
+    out
 }
