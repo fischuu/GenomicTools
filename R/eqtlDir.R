1239e90df2b42033d7d3c5ab48a5e061164e0dbd
fischuu <git@danielfischer.name>
2019-11-13 16:24:13 +0200

    getting closer to correct results

--- R/eqtlDir.R
+++ R/eqtlDir.R
@@ -1,37 +1,43 @@
 eqtlDir <- function(genoGroups,gex,mc=mc,nper, testType, MAF){
 
-  output <- c()
     innerFunction <- function(i)
     {
       # Calculate MAF
       maf.snp <- sum(genoGroups[genoGroups[,i]<3,i])/(2*(sum(genoGroups[,i]<3) ))
+      cat("maf.snp (a)", i, ":", maf.snp, "\n")
       maf.snp <- min(1-maf.snp, maf.snp)
+      cat("maf.snp (b)", i, ":", maf.snp, "\n")
       
       # Genotypes are coded as '3' if there is missing data, only proced if there is valid data
       if(sum(genoGroups[,i]==3)<(nrow(genoGroups))){
         missingData <- which((genoGroups[,i]==3)==TRUE)
-	PI <- length(table(genoGroups[genoGroups[,i]!=3,i]))
-	# All individuals have the same genotype, do nothing
-	if(PI==1){
-	  output[i] <- 1.2
+      	PI <- length(table(genoGroups[genoGroups[,i]!=3,i]))
+	    # All individuals have the same genotype, do nothing
+	      if(PI==1){
+	        innerOut <- 1.2
 	
-	# Then the 2 groups comparison
-	} else if (PI==2){
-	  if(testType=="asymptotic") testType <- "external"
-	  output[i] <- gmw(gex[genoGroups[,i]!=3],genoGroups[genoGroups[,i]!=3,i],test="mw",type=testType,alternative="two.sided",nper=nper)$p.values
+	      # Then the 2 groups comparison
+	      } else if (PI==2){
+	        if(testType=="asymptotic") testType <- "external"
+	        innerOut <- gmw(gex[genoGroups[,i]!=3],genoGroups[genoGroups[,i]!=3,i],test="mw",type=testType,alternative="two.sided",nper=nper)$p.values
 
-	# And the three group comparison
-	} else if (PI==3){
-	  p1 <- gmw(gex[genoGroups[,i]!=3],genoGroups[genoGroups[,i]!=3,i],test="triple",type=testType,alternative="greater",nper=nper)$p.values
-	  p2 <- gmw(gex[genoGroups[,i]!=3],createGroups(genoGroups[genoGroups[,i]!=3,i],c(2,1,0)),test="triple",type=testType,alternative="greater",nper=nper)$p.values
-	  output[i] <- min(2*min(p1,p2),1)
+	      # And the three group comparison
+	      } else if (PI==3){
+	         p1 <- gmw(gex[genoGroups[,i]!=3],genoGroups[genoGroups[,i]!=3,i],test="triple",type=testType,alternative="greater",nper=nper)$p.values
+	         p2 <- gmw(gex[genoGroups[,i]!=3],createGroups(genoGroups[genoGroups[,i]!=3,i],c(2,1,0)),test="triple",type=testType,alternative="greater",nper=nper)$p.values
+	         innerOut <- min(2*min(p1,p2),1)
 	  
-	}
+	      }
       } else {
-	output[i] <- 1.3
+        innerOut <- 1.3
       }
       
-    if(maf.snp<=MAF) output[i] <- 1.1  
+      if(maf.snp<=MAF){
+        innerOut <- 1.1
+        cat("Filtered SNP, as", maf.snp,"<", MAF, "\n")
+      }   
+      
+      innerOut
     }
     
   if(is.matrix(genoGroups)){
